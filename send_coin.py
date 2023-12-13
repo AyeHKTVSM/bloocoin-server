d303158f9e908673cebf2dac4bafb047659a78d3
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-13 05:17:42 -0500

    Update send_coin.py

--- send_coin.py
+++ send_coin.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-import mongo
+import mongo  # Ensure correct import as per your MongoDB driver
 import command
-
 
 class SendCoin(command.Command):
     """ Allows users to move coins from their address
-        to another users, given they have enough coins
+        to another user's address, provided they have enough coins
         for the transaction.
         This requires the ADDR and PWD from the bloostamp,
         which is the same as the ones sent to register.
 
-        fingerprint: {"cmd": "send_coin", "to": _, "addr": _, "pwd": _}
+        fingerprint: {"cmd": "send_coin", "to": _, "addr": _, "pwd": _, "amount": _}
     """
     required = ['amount', 'to', 'addr', 'pwd']
 
@@ -20,6 +18,7 @@ class SendCoin(command.Command):
         pwd = self.data['pwd']
         valid_account = mongo.db.addresses.find_one({"addr": addr, "pwd": pwd})
         valid_recipient = mongo.db.addresses.find_one({"addr": to})
+
         if not valid_account:
             self.error("Your address or password was invalid")
             return
@@ -31,15 +30,18 @@ class SendCoin(command.Command):
         if amount <= 0:
             self.error("Amount must be one or more")
             return
-        coins = mongo.db.coins.find({"addr": addr}).count()
+
+        coins = mongo.db.coins.count_documents({"addr": addr})
         if coins < amount:
             self.error("You don't have enough coins")
             return
-        for _ in xrange(0, amount):
-            before = mongo.db.coins.find_one({"addr": addr})
-            before['addr'] = to
-            mongo.db.coins.update({"addr": addr}, before)
-        mongo.db.transactions.insert({
+
+        for _ in range(amount):
+            coin = mongo.db.coins.find_one({"addr": addr})
+            coin['addr'] = to
+            mongo.db.coins.replace_one({"_id": coin["_id"]}, coin)
+
+        mongo.db.transactions.insert_one({
             "to": to,
             "from": addr,
             "amount": amount
