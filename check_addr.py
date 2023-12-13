3ae124f8189b952d3e1c051c6a10cb245009917b
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-13 05:08:17 -0500

    Update check_addr.py

--- check_addr.py
+++ check_addr.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
-import mongo
+import mongo  # Ensure correct import as per your MongoDB driver
 import command
 
 class CheckAddr(command.Command):
     """
-    
     {"cmd":"check_addr", "addr":"addr"}
-    
     """
     required = ['addr']
 
-    def handle(self, *args, **kargs):
+    def handle(self, *args, **kwargs):
         addr = self.data['addr']
         try:
-            amount = mongo.db.coins.find({'addr':addr}).count()
-            self.success({"addr":addr, "amount":amount})
-        except:
-            self.error("Error")
+            amount = mongo.db.coins.count_documents({'addr': addr})
+            self.success({"addr": addr, "amount": amount})
+        except Exception as e:  # Catch specific exceptions for better error handling
+            self.error(f"Error: {e}")
             return
