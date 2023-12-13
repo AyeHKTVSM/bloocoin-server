1a7e76578b4224167390db08cf1458345b2f9851
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-13 05:06:55 -0500

    Update check.py

--- check.py
+++ check.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-import mongo
+import mongo  # Ensure correct import as per your MongoDB driver
 from get_coin import difficulty
 from hashlib import sha512
 import command
-
 
 class Check(command.Command):
     """ Allows miners to send work to the
@@ -25,12 +23,16 @@ class Check(command.Command):
         winning_string = self.data['winning_string']
         winning_hash = self.data['winning_hash']
 
-        is_same_hash = winning_hash == sha512(winning_string).hexdigest()
+        is_same_hash = winning_hash == sha512(winning_string.encode()).hexdigest()
         coin_exists = mongo.db.coins.find_one({"hash": winning_hash})
         correct_diff = winning_hash.startswith(difficulty() * "0")
-        if is_same_hash and not coin_exists and correct_diff:
-            mongo.db.coins.insert({"hash": winning_hash, "addr": addr})
-            self.success({"hash": winning_hash})
-        else:
-            # Intentionally a bit vague, just so it's not infinitely easy
-            self.error("Something went wrong")
+
+        try:
+            if is_same_hash and not coin_exists and correct_diff:
+                mongo.db.coins.insert_one({"hash": winning_hash, "addr": addr})
+                self.success({"hash": winning_hash})
+            else:
+                # Intentionally a bit vague, just so it's not infinitely easy
+                self.error("Something went wrong")
+        except Exception as e:
+            self.error(f"Error: {e}")
