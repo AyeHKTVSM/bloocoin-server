1a1a2ef9665b9c894bcca93bec2a197bf8325dea
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-13 05:11:47 -0500

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
 import pymongo
 
-#Only change for remote authentication
-host = "localhost"
-port = 27017
-username = ""
-password = ""
+# Only change for remote authentication
+host = "your_remote_host"  # Replace with your remote host
+port = 27017  # Replace with your remote port
+username = "your_username"  # Replace with your username
+password = "your_password"  # Replace with your password
 
-db = pymongo.MongoClient(host, port).bloocoin
-#db.authenticate(username, password)
+client = pymongo.MongoClient(host, port, username=username, password=password)
+db = client.bloocoin
+
+# db.authenticate(username, password)  # Not required for recent versions of pymongo
+
+# Perform operations using the 'db' object as needed
