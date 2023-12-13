6125de4d596ed2f42ab53d43c314ac02861104ed
AyeHKTVSM <153189379+AyeHKTVSM@users.noreply.github.com>
2023-12-13 05:13:33 -0500

    Update register.py

--- register.py
+++ register.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
 import json
-
 import mongo
 import command
-
 
 class Register(command.Command):
     """ Allows clients to register their address with the
@@ -19,11 +16,17 @@ class Register(command.Command):
     def handle(self, *args, **kwargs):
         addr = self.data['addr']
         pwd = self.data['pwd']
+
         if len(addr) != 40:
             self.error("Registration unsuccessful")
             return
+
         if mongo.db.addresses.find_one({"addr": addr}):
             self.error("That address already exists")
             return
-        mongo.db.addresses.insert({"addr": addr, "pwd": pwd})
-        self.success({"addr": addr})
+
+        try:
+            mongo.db.addresses.insert_one({"addr": addr, "pwd": pwd})
+            self.success({"addr": addr})
+        except Exception as e:
+            self.error(f"Error occurred during registration: {e}")
